e1b440662f6e534109dc133cede0baf037e4a01e
dennerruby <dennersk891@gmail.com>
2020-12-09 22:22:21 -0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+
+  const {  REACT_APP_GOOGLE_API_KEY } = process.env;
+
+  useEffect(() =>{
+    setCurrentLocaction()
+  },[]);
+  
+
+async function setCurrentLocaction(){
+  await navigator.geolocation.getCurrentPosition(function(position){
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+
+  },function(error){
+    alert("Habilite a localização para usar esse APP")
+  });
 }
 
+
+  return (
+    <Fragment>
+        <LoadScript googleMapsApiKey={ REACT_APP_GOOGLE_API_KEY }>
+          <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}}
+            zoom={15}
+            center={{ lat: latitude, lng: longitude }}>
+            </GoogleMap>
+        </LoadScript>
+    </Fragment>
+)
+}
 export default App;
+ 
