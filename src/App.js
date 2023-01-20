a01e8815909278cb0b0c3b11a1139cc097f88436
Denner <denner@pop-os.localdomain>
2023-01-20 09:37:55 -0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,70 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import Establishment from './components/Establishment';
+import NearstCoffees from './components/NearstCoffees';
+import EstablishmentsService from './services/establishment_service'; 
 
 const App = () =>{
-  const [latitude, setLatitude] = useState(0)
-  const [longitude, setLongitude] = useState(0)
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [locations, setLocactions] = useState([]);
+  const [selected, setSelected] = useState({});
 
   const {  REACT_APP_GOOGLE_API_KEY } = process.env;
 
   useEffect(() =>{
     setCurrentLocaction()
-  },[]);
+  },[])
   
+
 
 async function setCurrentLocaction(){
   await navigator.geolocation.getCurrentPosition(function(position){
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
-
+    loadCoffeeShops();
   },function(error){
     alert("Habilite a localização para usar esse APP")
   });
 }
 
-
-  return (
-    <Fragment>
-        <LoadScript googleMapsApiKey={ REACT_APP_GOOGLE_API_KEY }>
-          <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}}
-            zoom={15}
-            center={{ lat: latitude, lng: longitude }}>
-            </GoogleMap>
-        </LoadScript>
-    </Fragment>
-)
+async function loadCoffeeShops(){
+  const response = await EstablishmentsService.index(latitude, longitude);
+  setLocactions(response.data.results);
 }
+return (
+  <Fragment>
+    <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
+      <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}}
+        zoom={15}
+        center={{lat: latitude, lng: longitude}}
+      >
+        {
+            locations.map((item, index) => {
+              return (
+                <Marker key={index} icon="/images/coffee-pin.png" title={item.name} animation="4" 
+                  position={{lat: item.geometry.location.lat, lng: item.geometry.location.lng}}
+                  onClick={() => setSelected(item)}
+                />
+          )
+        })
+        }
+        {
+          selected.place_id && (
+            <Establishment place={selected}/>
+          )
+        }
+        <Marker key="my location" icon="/images/my-location-pin.png" title="Seu local" animation="2"
+          position={{lat: latitude, lng: longitude}}
+        />
+
+      {(latitude != 0 && longitude != 0) &&
+   <NearstCoffees latitude={latitude} longitude={longitude} />
+     } 
+      </GoogleMap>
+    </LoadScript>
+  </Fragment>
+);
+}
+
 export default App;
- 
